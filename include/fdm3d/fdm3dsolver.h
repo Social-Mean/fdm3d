711fa2c82505d46ffdf30eb2cb788d244e18dab9
Social Mean <2052760@tongji.edu.cn>
2023-10-13 13:47:09 +0800

    添加项目文件。

---
+++ include/fdm3d/fdm3dsolver.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+
+class FDM3DSolver {
+public:
+	FDM3DSolver(int Nx, int Ny, int Nz);
+private:
+	
+};
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <fdm3d/fdm3dsolver.h>
+
+int main() {
+	std::cout << "fdm3d" << std::endl;
+}
